a0e3e9f757116742a72eb090de5464f7491cea30
JokkeXD <101457922+JokkeXD@users.noreply.github.com>
2024-04-28 14:51:18 +0300

    README.md update, create requirements.txt, update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import find_packages, setup
 
 setup(
     name="superkinodb",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         "flask",
+        "flask-caching",
         "flask-restful",
         "flask-sqlalchemy",
-        "SQLAlchemy",
+        "jsonschema",
+        "SQLAlchemy"
     ]
 )
